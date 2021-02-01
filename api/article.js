4a04f39f0c69ee579c8415c0bd42ebcb08f4982b
bruski <292803281@qq.com>
2021-02-01 08:08:48 +0800

    feat(api): add article & comment relavant api

--- api/article.js
+++ api/article.js
@@ -17,14 +17,6 @@ export const getArticle = (slug) => {
   });
 };
 
-// 获取文章的评论
-export const getArticleComments = (slug) => {
-  return request({
-    url: `/api/articles/${slug}/comments`,
-    method: "GET",
-  });
-};
-
 // 获取用户关注的文章
 export const getFeedArticles = (params) => {
   return request({
@@ -44,6 +36,72 @@ export const favoriteArticle = (slug) => {
 export const unfavoriteArticle = (slug) => {
   return request({
     url: `/api/articles/${slug}/favorite`,
+    method: "DELETE",
+  });
+};
+
+// 发表文章
+export const createArticle = (article) => {
+  return request({
+    url: "/api/articles",
+    method: "POST",
+    params: {
+      article: {
+        title: article.title,
+        description: article.description,
+        body: article.body,
+        tagList: Array.isArray(article.tagList) ? article.tagList : undefined, //  optional
+      },
+    },
+  });
+};
+
+// 更新文章
+export const updateArticle = (slug, article) => {
+  return request({
+    url: `/api/articles/${slug}`,
+    method: "PUT",
+    params: {
+      article: {
+        title: article.title, //  optional
+        description: article.description, //  optional
+        body: article.body, //  optional
+      },
+    },
+  });
+};
+
+// 删除文章
+export const deleteArticle = (slug) => {
+  return request({
+    url: `/api/articles/${slug}`,
+    method: "DELETE",
+  });
+};
+
+// 获取文章评论
+export const getArticleComments = (slug) => {
+  return request({
+    url: `/api/articles/${slug}/comments`,
+    method: "GET",
+  });
+};
+
+// 发表文章评论
+export const createArticleComment = (slug, body) => {
+  return request({
+    url: `/api/articles/${slug}/comments`,
+    method: "POST",
+    params: {
+      comment: { body },
+    },
+  });
+};
+
+// 删除文章评论
+export const deleteArticleComment = (slug, commentId) => {
+  return request({
+    url: `/api/articles/${slug}/comments/${commentId}`,
     method: "DELETE",
   });
 };
